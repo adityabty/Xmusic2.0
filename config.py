824332ae1f3b2616665abc2ae8467969e3435146
Xmusic <satyabhamabehera5931@gmail.com>
2025-11-06 18:15:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
+    "START_IMG_URL", "https://i.ibb.co/9kSr0qn7/x.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
@@ -122,5 +122,6 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
